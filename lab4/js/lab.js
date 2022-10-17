3ef4490e0624bd65503930f97aebe6ccdbe621b5
Egarcia42 <114507299+Egarcia42@users.noreply.github.com>
2022-10-17 11:13:26 -0700

    joe

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+@author EliGarcia.
+// Task 2: Psuedocoding the game Snake
+  // Create arena border
+  // Spawn the food pixel --> this becomes the objective
+  // Spawn snake entity (1 pixel/grid square big)
+  // When up, down, right or left key pressed, move the snake entity in the indicated direction
+  // Check if snake entity is touching arena border; if so, stop
+  // Check if snake entity is touching its own body; if so, stop
+  // Check if snake entity is touching food pixel; if so:
+    // Add an extra pixel on the end of the snake entity
+    // Make the food pixel disappear
+    // Spawn a new food pixel in a random location
+  //If snake entity is not touching food pixel, loop back to lines 6-9.
