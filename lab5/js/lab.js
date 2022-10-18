77b40083961ecf4e4587e1ffeddd2d9353e80591
Egarcia42 <114507299+Egarcia42@users.noreply.github.com>
2022-10-18 02:57:18 -0700

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -9,3 +9,13 @@ make = "Honda"
 model = "Civic"
 color = "purple"
 year = "2004"
+
+//calculate
+age = 2022 - year
+
+//output
+document.writeln("Make: " + make + "<br");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
