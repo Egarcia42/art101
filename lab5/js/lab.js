05edb8ef17856031116b4d6a0d200c69875628e9
Egarcia42 <114507299+Egarcia42@users.noreply.github.com>
2022-10-17 17:15:39 -0700

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,11 @@
+/**
+  * Author: Eli Garcia <egarc151@ucsc.edu>
+  * Created: 17 October
+  * License: Public Domain
+  **/
+
+// Define Variables
+make = "Honda"
+model = "Civic"
+color = "purple"
+year = "2004"
