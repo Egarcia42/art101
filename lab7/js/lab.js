cd2e732097d958e5abbf3caa1de1edd3bc5c81bf
Egarcia42 <114507299+Egarcia42@users.noreply.github.com>
2022-10-24 11:34:17 -0700

    Update lab.js

---
+++ lab7/js/lab.js
@@ -0,0 +1,17 @@
+/**
+  * Author: Eli Garcia <egarc151@ucsc.edu>
+  * Created: 24 October 2022
+  * License: Public Domain
+  **/
+
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  //makes all characters lowercase, splits the characters and returns as an array, sorts the given array, and then re-joins the array into a singular string again.
+  var nameSorted = userName.toLower().split('').sort().join('');
+  // returns the sorted string
+  return nameSorted
+
+}
