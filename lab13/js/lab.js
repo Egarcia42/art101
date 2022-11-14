f1345e38f7927704fe708349b315ae4e60b07233
Egarcia42 <114507299+Egarcia42@users.noreply.github.com>
2022-11-14 14:16:11 -0800

    complete overhaul: Lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -4,39 +4,28 @@
   * License: Public Domain
   **/
 
-// given a number and an object that looks like this:
-//          {3: "Fizz", 5: "Buzz", 7: "Boom"}
-// loops over the numbers and outputs the number and matching text
-// for factors
-function fizzBuzzBoom(maxNums, factorObj) {
-  // iterate over all of out numbers
-  for (var num=0; num<maxNums; num++) {
-    // reset output string
-    var outputStr = "";
-    // iterate over the factors we got from the html
-    for (var factor in factorObj) {
-      // check to see if this num is a multiple of factor
-      if (num % factor ==0 ){
-        // if yes, than add the text to output string
-        outputStr += factorObj[factor];
+function fizzBuzz(){
+
+  for (i=1; i <= 200; i++) {
+    var newStr = "";
+    if (i % 3 == 0) {
+      newStr = newStr + "Fizz";
       }
+    if (i % 5 == 0) {
+      newStr = newStr + "Buzz";
     }
-    //now if we have words in outputStr, format it like this " - FizzBuzz!"
-    if (outputStr) {
-      outputStr = " - " + outputStr + "!";
+    if (i % 7 == 0 ) {
+      newStr = newStr + "Boom";
     }
-    outputToPage(num.toString() + outputStr)
+    if (newStr.length > 0) {
+      $("#output").append("<p> "+newStr+" </p>");
+    }
+    else{
+      $("#output").append("<p> "+i+" </p>");
+    }
   }
 }
 
-function reportError(str) {
-  outputEl.innerHTML = "<div class = 'error'>" + str + "</div>";
-}
-
-document.getElementById("submit").addEventListeer("click", function(){
-  var max = document.getElementById("max").value;
-  console.log("max:",max)
-  if(! max) {
-    reportError("You must provide a maximum"):
-  }
-})
+$("#submit").click(function(){
+  fizzBuzz();
+});
